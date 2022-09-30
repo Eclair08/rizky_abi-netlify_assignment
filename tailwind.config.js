d03b9c018bf6505185e8b3cb8765805192fd58c2
Rizky Abi <rizkyabiwafa@gmail.com>
2022-09-30 14:10:39 +0700

    commit assignment

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.export = {
+    content: ['index.html'],
+    theme: {
+        center: true,
+        padding: '16px',
+    },
+    extend: {
+        screens: {
+            'tablet': '640px',
+            'laptop': '1024px',
+            '2xl': '1320px',
+        },
+    },
+};
